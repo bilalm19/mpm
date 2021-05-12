5aba02d33665d63b9ac112d45616934b8e0d29dd
bilalm19 <bilal@hazen.ai>
2021-05-13 02:12:12 +0500

    improved logging

--- logger/logger.go
+++ logger/logger.go
@@ -1,23 +1,37 @@
 package logger
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
-func Fatal(message interface{}) {
-	log.Fatalf("\033[1;31m[Fatal]\033[0m %s\n", message)
+func Fatal(message ...interface{}) {
+	fmt.Print(time.Now().Format("2006/01/02 15:04:05"))
+	fmt.Print(" \033[1;31m[Fatal]\033[0m ")
+	fmt.Fprintln(os.Stdout, message...)
+	os.Exit(1)
 }
 
-func Error(message interface{}) {
-	log.Printf("\033[1;31m[Error]\033[0m %s\n", message)
+func Error(message ...interface{}) {
+	fmt.Print(time.Now().Format("2006/01/02 15:04:05"))
+	fmt.Print(" \033[1;31m[Error]\033[0m ")
+	fmt.Fprintln(os.Stdout, message...)
 }
 
-func Info(message interface{}) {
-	log.Printf("\033[1;36m[Info]\033[0m %s\n", message)
+func Info(message ...interface{}) {
+	fmt.Print(time.Now().Format("2006/01/02 15:04:05"))
+	fmt.Print(" \033[1;36m[Info]\033[0m ")
+	fmt.Fprintln(os.Stdout, message...)
 }
 
-func Infof(message interface{}) {
-	log.Printf("\033[1;36m[Info]\033[0m %+v", message)
+func Infof(format string, message ...interface{}) {
+	fmt.Print(time.Now().Format("2006/01/02 15:04:05"))
+	fmt.Print(" \033[1;36m[Info]\033[0m " + fmt.Sprintf(format, message...))
 }
 
-func Warning(message interface{}) {
-	log.Printf("\033[1;33m[Warn]\033[0m %s\n", message)
+func Warning(message ...interface{}) {
+	fmt.Print(time.Now().Format("2006/01/02 15:04:05"))
+	fmt.Print(" \033[1;33m[Warn]\033[0m ")
+	fmt.Fprintln(os.Stdout, message...)
 }
